5f48a31e4998623c15edb5a497572d2667018a15
ShibbyMonster <42128795+ShibbyMonster@users.noreply.github.com>
2018-08-23 16:54:24 +1000

    Added PK's + new table
    Added PK's to temp,  hum and motion as the foreign key cannot be a PK as its 1..* relationship
    Added "Room" Table for sensors

--- CreateDB 1.0.sql
+++ CreateDB 1.0.sql
@@ -47,6 +47,7 @@ Foreign Key (CustomerID) References Customer (CustomerID) ON UPDATE CASCADE ON D
 GO
 
 CREATE TABLE Temperature (
+TempID INT PRIMARY KEY NOT NULL,
 SensorID	INT NOT NULL,
 Temp		DECIMAL(3,2),
 Date		TIMESTAMP,
@@ -55,6 +56,7 @@ Foreign Key (SensorID) References Sensor (SensorID) ON UPDATE CASCADE ON DELETE 
 GO
 
 CREATE TABLE Humidity (
+HumidityID INT PRIMARY KEY NOT NULL,
 SensorID	INT NOT NULL,
 Humidity	DECIMAL(3,2),
 Date		TIMESTAMP,
@@ -63,9 +65,19 @@ Foreign Key (SensorID) References Sensor (SensorID) ON UPDATE CASCADE ON DELETE 
 GO
 
 CREATE TABLE Motion (
+MotionID INT PRIMARY KEY NOT NULL,
 SensorID	INT NOT NULL,
 Motion		BIT,
 Date		TIMESTAMP,
+Foreign Key (SensorID) References Sensor (SensorID) ON UPDATE CASCADE ON DELETE NO ACTION
+)
+GO
+
+CREATE TABLE Room (
+RoomID INT PRIMARY KEY NOT NULL,
+Name VARCHAR(50),
+Description VARCHAR(200),
+SensorID INT NOT NULL,
 Foreign Key (SensorID) References Sensor (SensorID) ON UPDATE CASCADE ON DELETE NO ACTION
 )
 GO
