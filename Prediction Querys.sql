60c8fde39e1eeb02cc2e99b180868bba0f666f5e
FullMetalEd <FullMetalEd@users.noreply.github.com>
2018-11-08 20:03:39 +1100

    updated humidity analysis

--- Prediction Querys.sql
+++ Prediction Querys.sql
@@ -47,50 +47,39 @@ set @SearchStartTime = DATEADD(week,-1,@SearchEndTime)
 
 				
 ---- sql query for obtaing humidity predictions
-declare @SearchEndTime datetime;
-set @SearchEndTime = CURRENT_TIMESTAMP 
-declare @SearchStartTime datetime;
-set @SearchStartTime = DATEADD(week,-1,@SearchEndTime)
+DECLARE @SearchEndTime datetime;
+SET @SearchEndTime = CURRENT_TIMESTAMP 
+DECLARE @SearchStartTime datetime;
+SET @SearchStartTime = DATEADD(week,-1,@SearchEndTime)
 
-		SELECT StartTime, LAG(PredictedValue, 1, PredictedValue) OVER (ORDER BY StartTime) + (LAG(PredictedValue, 1, 0) OVER (ORDER BY StartTime) * (PercentChange)) as PredictedValue
-		From 
-			(
-				SELECT HourlyAverage, StartTime,  PercentChange, ID, HourlyAverage as PredictedValue
-				From
-					(
-						SELECT HourlyAverage, StartTime,- 1 * (1 - Lag(HourlyAverage, 1, 0) OVER (Order by StartTime) / HourlyAverage) AS PercentChange, ID, EndTime
-						From
-							(
-								SELECT AVG(Humidity) AS HourlyAverage, StartTime, ROW_NUMBER() OVER (ORDER BY StartTime) AS ID, EndTime
-									FROM
-										(
-											SELECT HumidityID, StartTime, Humidity, StartTime + '00:59:59' AS EndTime
-											FROM 
-												(
-													SELECT HumidityID, DATEADD(hh,DATEDIFF(hh,0,h.[Date]),0) AS StartTime, Humidity, s.SensorID
-													FROM Humidity h
-													INNER JOIN Sensor s ON  s.SensorID = h.SensorID
-													WHERE h.SensorID = 2 and  h.[Date] BETWEEN @SearchStartTime AND @SearchEndTime 
-													GROUP BY h.HumidityID, h.[Date], h.Humidity, s.SensorID	  
-												)	 
-												Humidity
-												INNER JOIN Sensor s ON s.SensorID = Humidity.SensorId 
-												GROUP BY HumidityID, StartTime, Humidity
-								)
-								Humidity
-								WHERE StartTime BETWEEN StartTime AND EndTime
-								GROUP BY StartTime, EndTime
-						)
-						Humidity
-						WHERE StartTime BETWEEN StartTime AND EndTime
-						Group By HourlyAverage, StartTime, ID, EndTime
-				)			
-				Humidity
-				GROUP BY HourlyAverage, StartTime, PercentChange, ID
-					
-			)
-			Humidity
-			Order by StartTime
+SELECT StartTime, PredictedValue = Case when PredictedValue1 >= 100 then 95 when PredictedValue1 < 100 then PredictedValue1 end 
+From (
+	SELECT StartTime, LAG(PredictedValue, 1, PredictedValue) OVER (ORDER BY StartTime) + (LAG(PredictedValue, 1, 0) OVER (ORDER BY StartTime) * (PercentChange)) as PredictedValue1
+	FROM (
+		SELECT HourlyAverage, StartTime,  PercentChange, ID, HourlyAverage as PredictedValue
+		FROM (
+			SELECT HourlyAverage, StartTime,- 1 * (1 - Lag(HourlyAverage, 1, 0) OVER (ORDER BY StartTime) / HourlyAverage) AS PercentChange, ID, EndTime
+			FROM (
+				SELECT AVG(Humidity) AS HourlyAverage, StartTime, ROW_NUMBER() OVER (ORDER BY StartTime) AS ID, EndTime
+				FROM (
+					SELECT HumidityID, StartTime, Humidity, StartTime + '00:59:59' AS EndTime
+					FROM (
+						SELECT HumidityID, DATEADD(hh,DATEDIFF(hh,0,h.[Date]),0) AS StartTime, Humidity, s.SensorID
+						FROM Humidity h
+						INNER JOIN Sensor s ON  s.SensorID = h.SensorID
+						WHERE h.SensorID > 1 and  h.[Date] BETWEEN @SearchStartTime AND @SearchEndTime 
+			GROUP BY h.HumidityID, h.[Date], h.Humidity, s.SensorID ) Humidity
+			INNER JOIN Sensor s ON s.SensorID = Humidity.SensorId 
+			GROUP BY HumidityID, StartTime, Humidity ) Humidity
+		WHERE StartTime BETWEEN StartTime AND EndTime
+		GROUP BY StartTime, EndTime) Humidity
+	WHERE StartTime BETWEEN StartTime AND EndTime
+	GROUP BY HourlyAverage, StartTime, ID, EndTime ) Humidity
+	GROUP BY HourlyAverage, StartTime, PercentChange, ID ) Humidity 
+)Humidity 
+GROUP BY StartTime, PredictedValue1
+ORDER BY StartTime
+
 		
 		
 
